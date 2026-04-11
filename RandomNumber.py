1dd94bebc363d2996c75b9cb91b33d4f620a4777
DarkWounds <darkwounds.james@gmail.com>
2026-04-11 19:34:04 +0300

    Curatare cod ghicire numar

--- RandomNumber.py
+++ RandomNumber.py
@@ -1,15 +1,18 @@
 import random
 numar = random.randint(1, 100)  # genereaza random intre 1 si 100
 
-nr = int(input("Introdu un numar intre 1 si 100: "))
+nrIncercari = 1
 
 print("=== Ghici numarul ===")
 
-while (nr != numar):
-    if (nr < numar):
+nr = int(input("Introdu un numar intre 1 si 100: "))
+
+while nr != numar:
+    if nr < numar:
         print("Numarul este prea mic")
     else:
         print("Numarul este prea mare")
+    nrIncercari += 1
     nr = int(input("Introdu un numar intre 1 si 100: "))
 
-print("Felicitari! Ai ghicit numarul!")    
+print(f"Felicitari! Ai ghicit numarul {numar} in {nrIncercari} incercari!")
